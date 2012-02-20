32758fa6ee25ca4b09b4eea95f93f47fc76daa52
SergeTruth <struth@securityinnovation.com>
2012-02-20 15:30:03 +0000

    + Merged Serge's and Dinis's content. + Removed duplicate tags. + Added Table of Contents. + Added links to the ToC to articles.

--- start webserver.bat
+++ start webserver.bat
@@ -1,4 +1,4 @@
 @echo off
-start "WebServer" "TeamMentor_master/WebServer/CassiniDev.exe" /port:12347 /portMode:Specific /path:"TeamMentor_master\Web Applications\TM_Website"
+start "WebServer" "TM_App/WebServer/CassiniDev.exe" /port:12347 /portMode:Specific /path:"TM_App\Web Applications\TM_Website"
 ping 123.45.67.89 -n 1 -w 3000 > nul
 start http://127.0.0.1:12347
