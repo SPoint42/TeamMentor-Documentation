a98772d63dc6ece122460e8f843e7de72cacca70
DinisCruz <dinis.cruz@owasp.org>
2012-02-24 17:59:59 +0000

    made table of contents to be always visible (as a left menu) testing css changes

--- TM_App/Web Applications/TM_Website/_Customizations/TM_Documentation/js/TM_Documentation.js
+++ TM_App/Web Applications/TM_Website/_Customizations/TM_Documentation/js/TM_Documentation.js
@@ -2,13 +2,15 @@
 TM.Documentation = 
     {
             contentDiv              : "#PageContent"
+		,	leftMenuDiv				: "#LeftMenu"
         ,   open_HomePage           : function() 
                                         {
                                             TM.Documentation.open_in_ContentDiv("Pages/HomePage.htm");
                                         }
         ,   open_TableOfContents    : function() 
                                         {
-                                            TM.Documentation.open_in_ContentDiv("Pages/TableOfContents.htm");
+                                            TM.Documentation
+											$(TM.Documentation.leftMenuDiv).load("Pages/TableOfContents.htm".add_TimeToUrl());
                                         }
         ,   open_in_ContentDiv      : function(page)
                                         {   
@@ -33,22 +35,30 @@ TM.Documentation =
                                                                          .appendTo(".nav")
                                                             }
                                             addLink("Home Page"             , this.open_HomePage);
-                                            addLink("Table of Contents"     , this.open_TableOfContents);                                            
+                                            //addLink("Table of Contents"     , this.open_TableOfContents);                                            
                                             addLink("This TeamMentor Website", this.open_TM_Website);
                                             addLink("SecurityInnovation.com", this.open_SI_Website);
 
                                         }
         ,   buildGui                : function()
-                                        {                                            
+                                        {   
+											
                                             TM.Documentation.add_LinksToMenu();
                                             TM.Documentation.open_HomePage();
-                                            //TM.Documentation.open_TableOfContents();
+											TM.Documentation.open_TableOfContents();                                            
 
-                                         
+											
+											
                                         }
-
+		,   cssFixes                : function()
+										{
+											$(".container").width("100%")
+											$("#LeftMenu").width(230);
+											$("#PageContent").absolute().left(400).top(60);
+										}
         ,   loadData_and_BuildGuid  : function()
                                         {
+											this.cssFixes();
                                             TM.WebServices.Data.extractFolderStructure(this.buildGui);
                                         }    
 
